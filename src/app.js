5d082bf9afa2b866bb5d253e4f14c0e655fdb3b9
kimmergithub <kimmer@atomicprofits.com>
2017-08-27 14:33:23 -0700

    light portfolio build

--- src/app.js
+++ src/app.js
@@ -3,7 +3,14 @@ const css = require('./app.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = () => {
+  return <h1 className="title">Kimmer Joseph OReilly</h1>
+}
+
 ReactDOM.render(
-  <h1>Kimmer Joseph O'Reilly</h1>,
+  <App />,
   document.getElementById('kimmer')
 );
+
+
+// <button>Home</button><button>Projects</button><button>Contact</button></div>,

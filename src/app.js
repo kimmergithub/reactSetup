1e8c31842471ea7b26a5c1862d88c3705b11d801
kimmergithub <kimmer@atomicprofits.com>
2017-09-04 11:56:55 -0700

    new direction

--- src/app.js
+++ src/app.js
@@ -1,25 +1,54 @@
-// Import modules === pulling in everything... 
+// Import modules === pulling in everything...
 import React from 'react';
 
-const Headline = () => {
-  return <h1 className="title">Kimmer Joseph OReilly</h1>
+constructor(){
+  super();
+  this.state = {
+    // App state goes here...
+  }
 }
 
-const NavButtons = () => {
+portNavButtons(event){
+  event.preventDefault();
+  const {}
+}
+
+export class App extends React.Component{
   return (
     <div>
-      <button className="nav-buttons" id="home-button">Home</button>
-      <button className="nav-buttons" id="portfolio-button">Portfolio</button>
-      <button className="nav-buttons" id="contact-button">Contact</button>
+    <button onClick={(event))} className="nav-buttons" id="home-button">Home</button>
+    <button className="nav-buttons" id="portfolio-button">Portfolio</button>
+    <button className="nav-buttons" id="contact-button">Contact</button>
     </div>
   )
 }
 
-export const App = () => {
-  return (
-    <div>
-      <Headline />
-      <NavButtons />
-    </div>
-  )
-}
+// complete this -- use rain volume --- start inplimenting other plugin -- bootstrap etc... 
+
+
+
+
+// const Headline = () => {
+//   return <h1 className="title">Kimmer Joseph OReilly</h1>
+// }
+
+// transfer show hide functionality
+// const NavButtons = () => {
+//   return (
+//     <div>
+//       <button onChange={this.onChange} className="nav-buttons" id="home-button">Home</button>
+//       <button className="nav-buttons" id="portfolio-button">Portfolio</button>
+//       <button className="nav-buttons" id="contact-button">Contact</button>
+//     </div>
+//   )
+// }
+
+
+// export const App = () => {
+    // return (
+    //   <div>
+    //   <Headline />
+    //   <NavButtons />
+    //   </div>
+    // )
+// }
